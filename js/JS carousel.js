b9f016100a187a547c2c3b39329722b3b18b5be0
rocha12334 <ryan.rocha05@escola.pr.gov.br>
2025-11-07 10:38:16 -0300

    carousel.js

--- js/JS carousel.js
+++
@@ -1,9 +0,0 @@
-var btn = document.querySelectorAll ('.listaDeartigos-slider-item');
-
-//percorrer todos os botoes controladores
-btn.forEach(function(btn) {
-    btn.addEventListener('click', function()) {
-
-
-    //Remover class 'ativo' dos outros botoes
-}
---
+++ js/carousel.js
@@ -0,0 +1,15 @@
+var btns = document.querySelectorAll('.listaDeArtigos-slider-item');
+
+//Percorrer todos os botoes controladores
+btns.forEach(function(btn) {
+    btn.addEventListener('click', function()) {
+
+
+        //Remover class 'ativo' dos outros botoes
+        btns.forEach(function(btnRemoveClass) {
+            btnRemoveClass.classList.remove('listaDeArtigos-slider-item-ativo')
+        })
+
+        this.classList.add('listaDeArtigo-slider-item--ativo')
+        })
+
