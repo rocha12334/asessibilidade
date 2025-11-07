b5f804f5cb1d68acc5b492a822fed1480c5b0b95
rocha12334 <ryan.rocha05@escola.pr.gov.br>
2025-11-07 08:39:49 -0300

    carousel.js

---
+++ js/JS carousel.js
@@ -0,0 +1,9 @@
+var btn = document.querySelectorAll ('.listaDeartigos-slider-item');
+
+//percorrer todos os botoes controladores
+btn.forEach(function(btn) {
+    btn.addEventListener('click', function()) {
+
+
+    //Remover class 'ativo' dos outros botoes
+}
